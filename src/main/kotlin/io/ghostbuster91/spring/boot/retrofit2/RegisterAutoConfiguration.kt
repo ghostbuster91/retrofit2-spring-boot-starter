bf26ade889a9715c444ef829e225b36358668618
ghostbuster91 <kghost0@gmail.com>
2018-11-07 22:33:52 +0100

    Export retrofitAnnotationProcessor as bean

--- src/main/kotlin/io/ghostbuster91/spring/boot/retrofit2/RegisterAutoConfiguration.kt
+++ src/main/kotlin/io/ghostbuster91/spring/boot/retrofit2/RegisterAutoConfiguration.kt
@@ -1,8 +1,16 @@
 package io.ghostbuster91.spring.boot.retrofit2
 
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import retrofit2.Retrofit
 
 @Configuration
 @Import(AutoRetrofitInterfaceScanner::class)
-class RegisterAutoConfiguration
+class RegisterAutoConfiguration{
+
+    @Bean
+    fun retrofitAnnotationProcessor(beanFactory: AutowireCapableBeanFactory, retrofit: Retrofit) =
+            RetrofitAnnotationProcessor(beanFactory, retrofit)
+}
